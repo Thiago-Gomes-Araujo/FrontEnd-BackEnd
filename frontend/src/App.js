b8af0c291354f28497aebfe031dc29fcc8c48c2a
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2021-02-09 22:16:05 -0300

    Atualizando Componente TasKForm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import Header from './components/Header';
-
 import TaskForm from './components/TaskForm';
-
 import TaskList from './components/TaskList';
+
 import './App.css';
 
 function App() {
@@ -11,11 +10,12 @@ function App() {
       <div className="app-wrapper">
         <Header />
         <div className="main">
-        <TaskForm />
-        <TaskList />
+          <TaskForm />
+          <TaskList />
         </div>
       </div>
     </div>
   );
 }
+
 export default App;
