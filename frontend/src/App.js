77b7b7eb4011334f0ea82ae68420003f4b3131c4
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2021-02-09 21:01:54 -0300

    Usando React

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className = "container">
+      <div className = "app-wrapper">
+        <header className = "header">
+          <h1>To do List</h1>
+        </header>
+      </div> 
+    </div>
+  );
+}
+export default App;
