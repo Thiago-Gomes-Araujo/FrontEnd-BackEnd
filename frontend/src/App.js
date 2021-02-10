6c4c81c677d6c1585bbef0e3e2ce5bd647fdc934
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2021-02-09 21:48:56 -0300

    atualizando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,34 @@
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className = "container">
-      <div className = "app-wrapper">
-        <header className = "header">
-          <h1>To do List</h1>
-        </header>
-      </div> 
+    <div className="container">
+      <div className="app-wrapper">
+        <Header />
+        <div className="main">
+          <form action="">
+            <input type="text" className="task-input"/>
+            <div className="buttons">
+              <button type="submit" className="btn add-task-btn">Adicionar</button>
+              <button className="btn clear-btn">Limpar</button>
+            </div>
+          </form>
+          <div>
+            <ul className="list-tasks">
+              <li className="list-item">
+                <span>Tarefa 1</span>
+                <div>
+                  <button>Deletar</button>
+                  <button>Editar</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default App;
