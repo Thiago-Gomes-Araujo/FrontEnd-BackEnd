d6126e5000d1257799d984fdf763948536f24c06
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2021-02-21 22:04:36 -0300

    adicionando componente Header

--- frontend/src/components/TaskForm/index.js
+++ frontend/src/components/TaskForm/index.js
@@ -1,15 +1,47 @@
-import './styles.css'
+import { useState } from 'react';
+import api from '../../service/api';
+import './styles.css';
 
 function TaskForm(){
-    return(
-        <form action="">
-        <input type="text" className="task-input"/>
-        <div className="buttons">
-          <button type="submit" className="btn add-task-btn">Adicionar</button>
-          <button className="btn clear-btn">Limpar</button>
-        </div>
-      </form>
-    );
+  const [title, setTitle] = useState("")
+  const [description, setdescription] = useState("")
+
+  const handleSubmit = async (event)=> {
+    event.preventDefaut()
+
+    try {
+      await api.post("/", {
+        title,
+        description
+      })
+      
+    } catch (error) {
+      console.log('Ocorreu um erro ao adicionar o Registro' + error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input 
+        type="text" 
+        className="task-input" 
+        placeholder="Add Titulo"
+        required
+        value={title}
+      />
+      <input 
+        type="text" 
+        className="task-input" 
+        placeholder="Add Descrição"
+        required
+        value={description}
+      />
+      <div className="buttons">
+        <button type="submit" className="btn add-task-btn">Adicionar</button>
+        <button className="btn clear-btn">Limpar</button>
+      </div>
+    </form>
+  );
 }
 
 export default TaskForm;
